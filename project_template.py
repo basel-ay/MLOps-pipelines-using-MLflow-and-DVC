5580343ba106a505f127d68a95664b0fddcf33b0
Basel Ayman <baselaymansalah@gmail.com>
2024-02-27 14:52:05 +0200

    Update project_template.py

--- project_template.py
+++ project_template.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import logging
 
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
 
 
 project_name = "MLOpsPipelines"
@@ -24,19 +24,15 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
-
-
+    "templates/index.html",
 ]
-
 
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-
-    if filedir !="":
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
@@ -44,7 +40,6 @@ for filepath in list_of_files:
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
-
 
     else:
         logging.info(f"{filename} is already exists")
