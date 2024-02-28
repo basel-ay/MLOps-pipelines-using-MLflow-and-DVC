a3154ea901950b23687ea555d223629157ffca99
Basel Ayman <baselaymansalah@gmail.com>
2024-02-28 23:25:20 +0200

    Update common.py

--- src/MLOpsPipelines/utils/common.py
+++ src/MLOpsPipelines/utils/common.py
@@ -4,6 +4,8 @@ import yaml
 from src.MLOpsPipelines import logger
 import json
 import joblib
+
+# To make sure that our code working fine, and if not then raise exceptions.
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
@@ -11,3 +13,79 @@ from typing import Any
 import base64
 
 
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """
+    Read a yaml file from the given path.
+
+    Args:
+        :path_to_yaml (str): Path to the yaml file.
+
+    Returns:
+        :ConfigBox: ConfigBox type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    """
+    Create list of directories.
+
+    Args:
+        :path_to_directories (list): list of path of directories.
+        :verbose (bool, optional): ignore log if multiple dirs is to be created. Defaults to False.
+
+    Returns:
+        None
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"created directory at: {path}")
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """
+    Save json data.
+
+    Args:
+        :path (Path): path to json file.
+        :data (dict): data to be saved in json file.
+
+    Returns:
+        None
+    """
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+
+    logger.info(f"json file saved at: {path}")
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """
+    Load json files data.
+
+    Args:
+        :path (Path): path to json file.
+
+    Returns:
+        :ConfigBox: data as class attributes instead of dict.
+    """
+    with open(path) as f:
+        content = json.load(f)
+
+    logger.info(f"json file loaded succesfully from: {path}")
+    return ConfigBox(content)
