90fa62842d3ed56c918f44024b8f58115d6b3c92
Basel Ayman <baselaymansalah@gmail.com>
2024-03-27 05:06:14 +0200

    Added the get_prepare_base_model_config to get the base model's configs and prams

--- src/MLOpsPipelines/config/configuration.py
+++ src/MLOpsPipelines/config/configuration.py
@@ -8,7 +8,10 @@ sys.path.append("/MLOps-pipelines-using-MLflow-and-DVC/")
 
 from src.MLOpsPipelines.constants import *
 from src.MLOpsPipelines.utils.common import read_yaml, create_directories
-from src.MLOpsPipelines.entity.config_entity import DataIngestionConfig
+from src.MLOpsPipelines.entity.config_entity import (
+    DataIngestionConfig,
+    PrepareBaseModelConfig,
+)
 
 
 class ConfigurationManager:
@@ -36,3 +39,23 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            # @configs
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            # @params
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES,
+        )
+
+        return prepare_base_model_config
