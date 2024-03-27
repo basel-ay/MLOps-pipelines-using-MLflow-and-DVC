ae0d21590ac8ea599af15adc486c11b74734b0ee
Basel Ayman <baselaymansalah@gmail.com>
2024-03-27 05:34:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from src.MLOpsPipelines import logger
-from src.MLOpsPipelines.pipeline.stage_01_data_ingestion import (
-    DataIngestionTrainingPipeline,
-)
+from src.MLOpsPipelines.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.MLOpsPipelines.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
-STAGE_NAME = "Data Ingestion stage"
+# STAGE_NAME = "Data Ingestion stage"
+
+
+# try:
+#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#     obj = DataIngestionTrainingPipeline()
+#     obj.main()
+#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+
+STAGE_NAME = "Prepare base model"
 
 
 try:
+    logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
