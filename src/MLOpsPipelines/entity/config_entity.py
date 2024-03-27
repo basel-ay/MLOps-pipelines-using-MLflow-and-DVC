09f94c3c8d2b9ea058f7c954afce0512036e0771
Basel Ayman <baselaymansalah@gmail.com>
2024-03-27 04:55:44 +0200

    Added PrepareBaseModelConfig class

--- src/MLOpsPipelines/entity/config_entity.py
+++ src/MLOpsPipelines/entity/config_entity.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 from pathlib import Path
 
 
+# Creating a dataclass with "frozen=True" means its instances are frozen and cannot be changed.
+
+
 @dataclass(frozen=True)
 class DataIngestionConfig:
     """
@@ -12,3 +15,18 @@ class DataIngestionConfig:
     source_url: str
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    """
+    PrepareBaseModelConfig is a dataclass that holds the configuration for our base model.
+    """
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
