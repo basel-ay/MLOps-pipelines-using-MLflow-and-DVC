86e703379fe8e2932358d80b51283802cbb4d823
Basel Ayman <baselaymansalah@gmail.com>
2024-04-06 06:23:40 +0200

    update

---
+++ script.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/basel-ay/MLOps-pipelines-using-MLflow-and-DVC.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"]="basel-ay"
+os.environ["MLFLOW_TRACKING_PASSWORD"]="a5a8b75b56d82fe87e2d51774739aed771e3313b"
